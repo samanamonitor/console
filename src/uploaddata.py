bf593268fd118bae959c454b5b4866d274fd716e
Fabian Baena <fabian.baena@samanagroup.co>
2018-06-15 11:35:05 -0400

    upload nagios plugin added

---
+++ src/uploaddata.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+sys.path.insert(0, './lib')
+from samananag import StatusDat
+
+datacenter_uuid = 'asdfa'
+url = 'http://nagios.samanagroup.com/samanamonitor/datacenter/' + datacenter_uuid
+
+s = StatusDat()
+s.parse()
+r = requests.put(url, data=json.dumps(s.data))
+
+if r.status_code == requests.code.ok:
+    print "OK - Data Uploaded"
+    exit(0)
+else:
+    print "CRITICAL - Unable to upload data"
+    exit(2)
